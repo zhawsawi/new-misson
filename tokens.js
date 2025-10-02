b33ed2ee353a7d58e8ffbd024543824d2c9e6b71
zhawsawi <kilyanmbappezeko@gmail.com>
2025-10-02 21:11:00 +0300

    Create tokens.js

---
+++ tokens.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    channelId: "1413287404252696768",
+    serverId: "248216205943373835",
+    token: process.env.token1,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+  {
+    channelId: "1413287404252696768",
+    serverId: "248216205943373835",
+    token: process.env.token2,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+  {
+    channelId: "1413287404252696768",
+    serverId: "248216205943373835",
+    token: process.env.token3,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+   {
+    channelId: "1395923052545577040",
+    serverId: "1394436161182629998",
+    token: process.env.token4,
+    selfDeaf: false,
+    autoReconnect: {
+      enabled: true,
+      delay: 30000,
+      maxRetries: 5,
+    },
+    presence: {
+      status: "invisible",
+    },
+    selfMute: false,
+  },
+];
